e55e556ec255cb5be19499af48f5006b4af41b2e
Zeynep Özdemir Açıkgöz <zeynmirgmail.com>
2022-07-01 15:29:06 +0300

    Initial Commit

---
+++ InstaFirebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstaFirebase
+//
+//  Created by Zeynep Özdemir Açıkgöz on 1.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
