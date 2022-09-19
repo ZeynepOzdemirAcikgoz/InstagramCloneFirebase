4d6010ef9487488c8724085735221bd521274d91
Zeynep Özdemir Açıkgöz <zeynmirgmail.com>
2022-09-19 09:32:19 +0300

    first commit

---
+++ InstaFirebase/FeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCell.swift
+//  InstaFirebase
+//
+//  Created by Zeynep Özdemir Açıkgöz on 4.07.2022.
+//
+
+import UIKit
+
+class FeedCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
